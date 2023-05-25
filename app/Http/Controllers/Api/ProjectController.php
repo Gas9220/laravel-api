6aac09fd9471e77caf616a2bb8b3679947447053
Gaspare <Gas9220@users.noreply.github.com>
2023-05-25 15:10:39 +0200

    Api Project controller

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index() {
+        $projects = Project::all();
+
+        return response()->json([
+            'success' => true,
+            'result' => $projects
+        ]);
+    }
+}
