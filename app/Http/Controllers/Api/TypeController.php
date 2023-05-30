3b954182428488f83e1e0c4fac7cd63ae336eed6
Gaspare <Gas9220@users.noreply.github.com>
2023-05-30 13:36:08 +0200

    Api TypeController with index and show methods

---
+++ app/Http/Controllers/Api/TypeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Type;
+
+class TypeController extends Controller
+{
+    public function index()
+    {
+        $types = Type::all();
+
+        return response()->json([
+            'success' => true,
+            'results' => $types
+        ]);
+    }
+
+    public function show(string $slug){
+        $types = Type::where('slug', $slug)->with('projects')->first();
+
+        return response()->json([
+            'success' => true,
+            'results' => $types
+        ]);
+
+    }
+}
