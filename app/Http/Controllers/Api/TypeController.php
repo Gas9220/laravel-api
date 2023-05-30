6f3eb26c0dc8a0355af972137aa0529d08b29594
Gaspare <Gas9220@users.noreply.github.com>
2023-05-30 13:54:05 +0200

    fix TypeController index method

--- app/Http/Controllers/Api/TypeController.php
+++ app/Http/Controllers/Api/TypeController.php
@@ -9,7 +9,7 @@ class TypeController extends Controller
 {
     public function index()
     {
-        $types = Type::all();
+        $types = Type::with('projects')->get();
 
         return response()->json([
             'success' => true,
