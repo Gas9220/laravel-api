448d59bc983c96e59a4ceec88fdf5bba526e0ade
Gaspare <Gas9220@users.noreply.github.com>
2023-05-30 18:02:57 +0200

    Mail - NewMessage

--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -3,8 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Message;
-use App\Models\Project;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class MessageSeeder extends Seeder
