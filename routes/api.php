edcea9436aa0afc9e6bea0886c025112a7dbb890
Gaspare <Gas9220@users.noreply.github.com>
2023-05-30 16:48:34 +0200

    TechnologyController routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\TechnologyController;
 use App\Http\Controllers\Api\TypeController;
 use Illuminate\Support\Facades\Route;
 
@@ -24,4 +25,7 @@ Route::get('projects/{id}', [ProjectController::class, 'show']);
 
 Route::get('types', [TypeController::class, 'index']);
 Route::get('types/{slug}', [TypeController::class, 'show']);
+
+Route::get('technologies', [TechnologyController::class, 'index']);
+Route::get('technologies/{slug}', [TechnologyController::class, 'show']);
 
